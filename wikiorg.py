2493e0023c371c4cec9d496a4a5e23a0cc2a000b
Oliver Gerlich <oliver.gerlich@gmx.de>
2008-04-11 21:45:00 +0200

    re-enable Markdown conversion, and display example.markdown at start

--- wikiorg.py
+++ wikiorg.py
@@ -19,13 +19,15 @@ class WikiOrgGui:
 
         self.tree.signal_autoconnect(self)
 
-        viewer = HtmlViewer()
-        viewer.setLinkHandler(self.linkHandler)
-        viewer.setHTML("<h1>HTML viewer test</h1>\n<p>This is a test text; and a <a href='http://google.de'>link</a></p>");
-        viewer.getWidget().show()
+        self.viewer = HtmlViewer()
+        self.viewer.setLinkHandler(self.linkHandler)
+        self.viewer.setHTML("<h1>HTML viewer test</h1>\n<p>This is a test text; and a <a href='http://google.de'>link</a></p>");
+        self.viewer.getWidget().show()
 
         viewerParent = self.tree.get_widget('mainScrollWin')
-        viewerParent.add(viewer.getWidget())
+        viewerParent.add(self.viewer.getWidget())
+
+        self.displayMarkdown('example.markdown')
 
     def linkHandler (self, url):
         print "link clicked (%s)" % url
@@ -33,23 +35,23 @@ class WikiOrgGui:
             wikiword = url[7:]
             filename = "./%s.markdown" % wikiword
             print "(internal link to '%s'; file: '%s')" % (wikiword, filename)
-            #displayMarkdown(viewer, filename)
+            self.displayMarkdown(filename)
         else:
             os.system("xdg-open '%s' &" % url)
 
-# TODO: maybe use another syntax for wiki links.
-# See http://boodler.org/wiki/show/Markdown/ for an idea
-def convertWikiLinks (html):
-    newHtml = re.sub("\[\[([\w ]+)\]\]",
-        lambda x: "<a href='wiki://"+x.groups()[0]+"'>"+x.groups()[0]+"</a>", html)
-    return newHtml
+    # TODO: maybe use another syntax for wiki links.
+    # See http://boodler.org/wiki/show/Markdown/ for an idea
+    def convertWikiLinks (self, html):
+        newHtml = re.sub("\[\[([\w ]+)\]\]",
+            lambda x: "<a href='wiki://"+x.groups()[0]+"'>"+x.groups()[0]+"</a>", html)
+        return newHtml
 
-def displayMarkdown (viewer, filename):
-    cmd = "perl Markdown.pl < '%s'" % filename
-    html = os.popen(cmd).read()
-    if (html):
-        html = convertWikiLinks(html)
-        viewer.setHTML(html)
+    def displayMarkdown (self, filename):
+        cmd = "perl Markdown.pl < '%s'" % filename
+        html = os.popen(cmd).read()
+        if (html):
+            html = self.convertWikiLinks(html)
+            self.viewer.setHTML(html)
 
 if __name__ == "__main__":
     #viewer = HtmlViewer()
