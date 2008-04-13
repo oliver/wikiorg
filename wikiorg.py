cd93d0d6f791ce19130a902bf7e63362b989f1cf
Oliver Gerlich <oliver.gerlich@gmx.de>
2008-04-13 22:01:17 +0200

    move FileStorageBackend class to own .py file

--- wikiorg.py
+++ wikiorg.py
@@ -9,7 +9,7 @@ import gobject
 import pango
 
 from htmlviewer import HtmlViewer
-
+from filebackend import FileStorageBackend
 
 class LinkHistory:
     """
@@ -91,40 +91,6 @@ class LinkHistory:
             self.gui.displayMarkdown( self.getCurrentPage() )
         else:
             print "warning: tried to go forward in history but there are not enough newer pages"
-
-
-class FileStorageBackend:
-    """ Handles the actual storage of wiki pages to files. """
-    def __init__ (self):
-        self.directory = "." # the directory where files are saved
-
-    def pageExists (self, pagename):
-        filename = self.pageToFile(pagename)
-        # TODO: check for directory, and regular file, and whatnot...
-        return os.path.exists(filename)
-
-    def createNewPage (self, pagename):
-        filename = self.pageToFile(pagename)
-        f = open(filename, "w")
-        f.write("enter text for %s here..." % pagename)
-        f.close()
-
-    def getPageContent (self, pagename):
-        filename = self.pageToFile(pagename)
-        f = open(filename, "r")
-        text = f.read()
-        f.close()
-        return text
-
-    def savePageContent (self, pagename, text):
-        filename = self.pageToFile(pagename)
-        f = open(filename, "w")
-        f.write(text)
-        f.close()
-
-    def pageToFile (self, wikiword):
-        """ Translates a page name (wiki word) into a file name. """
-        return "%s/%s.markdown" % (self.directory, wikiword)
 
 
 class WikiOrgGui:
